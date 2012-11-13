f12e16ffc5c2eba221a7ddea65b1083a2031ac6b
Yasser Souri <yassersouri@gmail.com>
2012-11-13 14:14:48 +0330

    remove the Phone attribute

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,46 @@
 import java.io.*;
 
+import weka.core.Attribute;
+import weka.core.Instances;
+import weka.core.converters.ArffLoader.ArffReader;
+import weka.filters.Filter;
+import weka.filters.unsupervised.attribute.Remove;
+
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Machine Learning - Programming Assignment 1");
 		
 		try {
 			BufferedReader reader = new BufferedReader(new FileReader("data/input/churn.arff"));
+			ArffReader arffReader = new ArffReader(reader);
+			Instances instances = arffReader.getData();
+			instances.setClassIndex(instances.numAttributes()-1);
+			
+			Remove filter = new Remove();
+			filter.setInvertSelection(false);
+			filter.setAttributeIndices("4");
+			try {
+				filter.setInputFormat(instances);
+			} catch (Exception e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			
+			
+			Instances newData = null;
+			try {
+				newData = Filter.useFilter(instances, filter);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			
 			
 		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
