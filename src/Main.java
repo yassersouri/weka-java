3b6a09eadedca0da8082a922403b4d9232b3fea5
Yasser Souri <yassersouri@gmail.com>
2012-11-13 13:06:57 +0330

    add weka.jar to classpath

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
+import java.io.*;
 
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Machine Learning - Programming Assignment 1");
+		
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader("data/input/churn.arff"));
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
