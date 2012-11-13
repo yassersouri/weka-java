fb9d342418a04b142d7300b1f63f5b81dd67dc72
Yasser Souri <yassersouri@gmail.com>
2012-11-13 22:24:38 +0330

    add more functions to util to help on filtering
    also make removing the attribute function more reusable

--- src/Main.java
+++ src/Main.java
@@ -9,17 +9,26 @@ public class Main {
 		try {
 			BufferedReader reader = new BufferedReader(new FileReader("data/input/churn.arff"));
 			ArffReader arffReader = new ArffReader(reader);
-			Instances instances = arffReader.getData();
-			instances.setClassIndex(instances.numAttributes()-1);
+			Instances data = arffReader.getData();
+			data.setClassIndex(data.numAttributes()-1);
 			
-			Instances data = null;
 			try {
-				data = Utils.removePhoneAttribute(instances);
+				// Remove Phone
+				data = Utils.removeAttribute(data, "4");
+				
+				// Account Length
+				data = Utils.discretizeAttribute(data, "2", 10);
+				
+				// Area Code
+				data = Utils.numericToNominal(data, "3");
+				
+				
+				
+				
 			} catch (Exception e) {
-				System.out.println("Removing Phone Attribute Failed!");
+				System.err.println("Filtering Attributes Failed!");
 				e.printStackTrace();
 			}
-			
 			
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
--- src/Utils.java
+++ src/Utils.java
@@ -1,15 +1,32 @@
 import weka.core.Instances;
 import weka.filters.Filter;
 import weka.filters.unsupervised.attribute.Remove;
-
+import weka.filters.unsupervised.attribute.Discretize;
+import weka.filters.unsupervised.attribute.NumericToNominal;
 
 public class Utils {
-	public static String INDEX_OF_PHONE_ATTRIBUTE = "4";
-	public static Instances removePhoneAttribute(Instances instances) throws Exception{
+	
+	public static Instances removeAttribute(Instances instances, String attribute) throws Exception{
 		Remove filter = new Remove();
 		filter.setInvertSelection(false);
-		filter.setAttributeIndices(INDEX_OF_PHONE_ATTRIBUTE);
+		filter.setAttributeIndices(attribute);
 		filter.setInputFormat(instances);
 		return Filter.useFilter(instances, filter);
+	}
+	
+	public static Instances discretizeAttribute(Instances data, String attribute, int numBins) throws Exception{
+		Discretize filter = new Discretize();
+		filter.setAttributeIndices(attribute);
+		filter.setBins(numBins);
+		filter.setMakeBinary(false);
+		filter.setInputFormat(data);
+		return Filter.useFilter(data, filter);
+	}
+	
+	public static Instances numericToNominal(Instances data, String attribute) throws Exception{
+		NumericToNominal filter = new NumericToNominal();
+		filter.setAttributeIndices(attribute);
+		filter.setInputFormat(data);
+		return Filter.useFilter(data, filter);
 	}
 }
